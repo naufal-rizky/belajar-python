dc4507349284863f9f9ea471bd43b84fec5326a5
naufal-rizky <naufalrizky.nav@gmail.com>
2022-10-28 10:08:42 +0700

    first commit

---
+++ Jobsheet 9/program2.py
@@ -0,0 +1,5 @@
+bil = 12345
+print('123457890123456789')
+print(bil)
+print('%6d'%bil)
+print('%12d'%bil)
---
+++ Jobsheet 9/program3.py
@@ -0,0 +1,5 @@
+bil = 123.456
+print('12345678901234567890')
+print('%6.1f' %bil)
+print('%8.2f' %bil)
+print('%7.3f' %bil)
