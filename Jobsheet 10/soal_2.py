b8d57ad293cce98a0da59042db058c82b794057e
naufal-rizky <naufalrizky.nav@gmail.com>
2022-11-09 17:08:45 +0700

    Update : Soal 2

--- Jobsheet 10/soal_2.py
+++ Jobsheet 10/soal_2.py
@@ -1,7 +1,20 @@
-print('=== PROGRAM TAHUN KABISAT ===')
-print('')
+import os
+
+os.system("cls")
+
+def tahunKabisat(tahun):
+    if tahun % 400 == 0 and tahun % 100 == 0:
+        return True
+    elif tahun % 4 == 0 and tahun % 100 != 0:
+        return True
+    else:
+        return False
+
+print("=== PROGRAM TAHUN KABISAT ===")
+
 tahun = int(input('Masukan Tahun = '))
-if tahun % 4 == 0 and tahun % 400 == 0:
-    print('%d adalah tahun kabisat' %tahun)
+
+if(tahunKabisat(tahun)):
+    print("{} adalah tahun kabisat" .format(tahun))
 else:
-    print('%d bukan tahun kabisat' %tahun)
+    print("{} bukan tahun kabisat" .format(tahun))
